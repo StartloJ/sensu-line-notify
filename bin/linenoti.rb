8cb1bcb23946df4a0600f370709e915368a89553
watcharin <watcharin@opsta.io>
2019-04-11 14:17:12 +0700

    delete useless file

--- bin/linenoti.rb
+++
@@ -1,8 +0,0 @@
-require 'json'
-require 'line_notify'
-
-@line_notify = LineNotify.new(get_token)
-options = {
-  message: "#{action_to_string} - #{event_name}: #{@event['check']['notification']}",
-}
-@line_notify.send(options)
