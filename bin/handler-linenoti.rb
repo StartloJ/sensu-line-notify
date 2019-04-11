108ed1102dc13d5bc82d4ae9faa869baacd6d1df
watcharin <watcharin@opsta.io>
2019-04-11 12:08:37 +0700

    fix whitespace in file

--- bin/handler-linenoti.rb
+++ bin/handler-linenoti.rb
@@ -13,37 +13,36 @@ require 'sensu-handler'
 
 class LinenotiHandler < Sensu::Handler
 
-	option :json_config,
+  option :json_config,
         description: 'Configuration label name',
         short: '-j JSONCONFIG',
         long: '--json JSONCONFIG',
         default: 'line'
-	
-	def action_to_string
-		@event['action'].eql?('resolve') ? 'RESOLVE' : 'ALERT'
-	end
-	
-	def event_name
+  
+  def action_to_string
+    @event['action'].eql?('resolve') ? 'RESOLVE' : 'ALERT'
+  end
+  
+  def event_name
     @event['client']['name'] + '/' + @event['check']['name']
-	end
+  end
 
-	def get_token
-		get_setting(line_token)
+  def get_token
+    get_setting(line_token)
 
-	def get_setting(name)
-		setting[config[:json_config]][name]
-	rescue TypeError, NoMethodError => e
+  def get_setting(name)
+    setting[config[:json_config]][name]
+  rescue TypeError, NoMethodError => e
     puts "settings: #{settings}"
     puts "error: #{e.message}"
     exit 3 # unknown
-	end
+  end
 
-	def handle
-		puts "something"
-		line_notify = LineNotify.new(get_token)
-		options = {
-				message: "#{action_to_string} - #{event_name}: #{@event['check']['notification']}",
-		}
-		line_notify.send(options)
-	end
+  def handle
+    @line_notify = LineNotify.new(get_token)
+    options = {
+        message: "#{action_to_string} - #{event_name}: #{@event['check']['notification']}",
+    }
+    @line_notify.send(options)
+  end
 end
